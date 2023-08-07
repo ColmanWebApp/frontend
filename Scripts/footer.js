ec1c03e0cac3280e0a59a1e14e66f0ce07a5b716
Tal Mekler <tal.mekler11@gmail.com>
2023-08-07 19:07:31 +0300

    footer done

--- Scripts/footer.js
+++ Scripts/footer.js
@@ -1,5 +1,13 @@
 const createFooter = ()=> {
-    document.querySelector('#footer').innerHTML ="<footer class='container-fluid px-5 py-4 d-flex align-items-center justify-content-between'><a class='text-light text-decoration-none fs-3 fw-bold' href='#'>MusicApp</a><p class='copyright-text p-0 m-0'>2023 © All Right Reserved To Yuval Ofek, Tal Mekler, Omer Ben David, Liav Burger</p><a href='#navbar' class='btn btn-outline-light rounded-pill'>Back To Top<i class='fa-solid fa-chevron-up ms-2'></i></a></footer>"
+    document.querySelector('#footer').innerHTML =
+    `<footer class="container-fluid px-5 py-4 d-flex align-items-center justify-content-between">
+    <a class="text-light text-decoration-none fs-3 fw-bold" href="#">MusicApp</a>
+    <p class="copyright-text p-0 m-0">2023 © All Right Reserved <span class="d-lg-inline d-none">To Yuval Ofek, Tal Mekler, Omer Ben David and Liav Burger</span></p>
+    <a href="#navbar" class="btn" id="back-to-top-btn">
+      Back To Top
+      <i class="fa-solid fa-chevron-up ms-2"></i>
+    </a>
+  </footer>`
 }
 
 createFooter();
