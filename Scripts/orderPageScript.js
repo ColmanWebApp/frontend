96b5f593f6ed5458437d75710c22c1ea8c45aa47
Tal Mekler <tal.mekler11@gmail.com>
2023-08-08 10:29:30 +0300

    fix ui bugs

--- Scripts/orderPageScript.js
+++ Scripts/orderPageScript.js
@@ -9,7 +9,7 @@ const addListItem = (imgUrl, title, price, itemID) => {
             <img class="w-100" src=${imgUrl} alt="">
         </div>
         <div class="col-9 px-4 d-flex h-100 align-items-center justify-content-between">
-            <span class="list-item-title fw-semibold text-light">${title}</span>
+            <span class="list-item-title fw-semibold text-light col-6 col-md-7 col-lg-9">${title}</span>
             <span>
                 <span class="price me-3">${price}$</span>
                 <i class="fa-solid fa-trash" attr_id=${itemID} onclick="onDeleteFromCart(this)"></i>
@@ -35,20 +35,22 @@ const onDeleteFromCart = (event)=> {
 const createCartList = ()=> {
     const list = document.querySelector("#cart-list");
     list.innerHTML = ""
-    const numOfListItems = 0;
+    const numOfListItems = 3; // todo: get the value from local-storage
 
     if(numOfListItems === 0) { // cart is empty
-        list.innerHTML = `<div class="alert alert-primary" role="alert">
-        Your cart is empty. Please start shopping to add items.
-    </div>`
-    fillOrderSummary(0,0,"Free",0)
+        list.innerHTML = 
+        `<div class="alert alert-primary" role="alert">
+            Your cart is empty. Please start shopping to add items.
+        </div>`
+        fillOrderSummary(0,0,"Free",0)
     }else {
         let subtotal = 0
         let discount = 0
         let shipping = 0
         for (let index = 0; index < numOfListItems; index++) {
-            addListItem("https://picsum.photos/200", "Song Title", 19.99, index)
-            subtotal += 19.99
+            let itemPrice = 19.99 + index
+            addListItem("https://picsum.photos/200", "very long song title with a lot of words", itemPrice, index)
+            subtotal += itemPrice
         }
         subtotal = subtotal.toFixed(2)
         fillOrderSummary(numOfListItems, subtotal, shipping === 0 ? "Free" : shipping.toFixed(2), subtotal - discount + shipping);
